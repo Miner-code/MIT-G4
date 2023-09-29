953b1cef491ac7d169bf23852d721352a633e643
ioamra <iom.monchau@gmail.com>
2023-09-29 15:58:33 +0200

    UPDATE BDD

--- bdd_unea.sql
+++ bdd_unea.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 27 sep. 2023 à 13:32
--- Version du serveur : 5.7.36
--- Version de PHP : 7.4.26
+-- Généré le : ven. 29 sep. 2023 à 13:57
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -49,7 +49,15 @@ CREATE TABLE IF NOT EXISTS `cursus` (
   `libelle_cursus` varchar(250) DEFAULT NULL,
   `spe_cursus` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_cursus`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `cursus`
+--
+
+INSERT INTO `cursus` (`id_cursus`, `libelle_cursus`, `spe_cursus`) VALUES
+(1, 'BTS SIO', 'SLAM'),
+(2, 'BTS SIO', 'SISR');
 
 -- --------------------------------------------------------
 
@@ -62,8 +70,8 @@ CREATE TABLE IF NOT EXISTS `etablissement` (
   `id_etab` int(11) NOT NULL AUTO_INCREMENT,
   `nom_etab` varchar(200) DEFAULT NULL,
   `adresse_etab` varchar(250) DEFAULT NULL,
-  `profil_etab` varchar(500) NOT NULL,
-  `banniere_etab` varchar(500) NOT NULL,
+  `profil_etab` varchar(500) DEFAULT NULL,
+  `banniere_etab` varchar(500) DEFAULT NULL,
   `id_ville` int(11) NOT NULL,
   PRIMARY KEY (`id_etab`),
   KEY `id_ville` (`id_ville`)
@@ -105,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `participer` (
   KEY `id_user` (`id_user`),
   KEY `id_cursus` (`id_cursus`),
   KEY `id_etab` (`id_etab`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -133,11 +141,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `prenom_user` varchar(50) DEFAULT NULL,
   `mail_user` varchar(50) DEFAULT NULL,
   `dtn_user` date DEFAULT NULL,
-  `mdp_user` varchar(50) DEFAULT NULL,
+  `mdp_user` varchar(64) DEFAULT NULL,
   `img_user` varchar(250) DEFAULT NULL,
   `role_user` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nom_user`, `prenom_user`, `mail_user`, `dtn_user`, `mdp_user`, `img_user`, `role_user`) VALUES
+(1, 'Admin', 'Admin', 'admin@gmail.com', '1999-01-01', 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9', 'upload/user/defaut.png', 1);
 
 -- --------------------------------------------------------
 
@@ -151,7 +166,7 @@ CREATE TABLE IF NOT EXISTS `ville` (
   `nom_ville` varchar(50) DEFAULT NULL,
   `cp_ville` decimal(5,0) DEFAULT NULL,
   PRIMARY KEY (`id_ville`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Contraintes pour les tables déchargées
