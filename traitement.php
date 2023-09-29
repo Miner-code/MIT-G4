73bcbab8025fadec6c8466a94d3e7bccd53f46d8
Warzaros <65943109+Warzaros@users.noreply.github.com>
2023-09-29 11:32:16 +0200

    traitement_new

--- traitement.php
+++ traitement.php
@@ -1,31 +1,28 @@
 <?php
-    session_start();
-    include "include/bdd.php";
+session_start();
+include "include/bdd.php";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["message"])) {
-            $message = $_POST["message"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["message"])) {
+        $message = $_POST["message"];
 
-            // On remplace "l'id_entreprise" par le champ approprié de votre table
-            // qui stocke l'ID de l'entreprise.
-            $id_entreprise = 1;
+        $id_etab = 1; 
 
-            // Insérez le message dans la table des impression
-            $sql = "INSERT INTO impression (titre, contenu, num_date, num_1, num_user)
-                    VALUES (:titre, :contenu, NOW(), :num_1, :num_user)";
-            $stmt = $bdd->prepare($sql);
-            $stmt->bindParam(":titre", $_SESSION["nom_entreprise"]);
-            $stmt->bindParam(":contenu", $message);
-            $stmt->bindParam(":num_1", $id_entreprise);
-            $stmt->bindParam("num_user", $_SESSION["id_utilisateur"]); // Remplacez par l'ID de l'utilisateur connecté
+        
+        $sql = "INSERT INTO impression (titre_imp, contenu_imp, date_imp, id_theme, id_user)
+                VALUES (:titre, :contenu, NOW(), :id_etab, :id_user)";
+        $stmt = $bdd->prepare($sql);
+        $stmt->bindParam(":titre", $_SESSION["nom_etab"]); 
+        $stmt->bindParam(":contenu", $message);
+        $stmt->bindParam(":id_etab", $id_etab);
+        $stmt->bindParam(":id_user", $_SESSION["id_user"]); 
 
-            if($stmt->execute()) {
-                // On redirige l'utilisateur vers la page d'acceuil ou une autre page de confirmation
-                header("Location: index.php");
-                exit();
-            } else {
-                echo "Une erreur s'est produite lors de la publication";
-            }
+        if ($stmt->execute()) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "An error occurred while posting.";
         }
     }
+}
 ?>
