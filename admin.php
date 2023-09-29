9f377834e184ab9d0a5e34ade40225bb9e4413c5
Alangithhub <riehlalan@gmail.com>
2023-09-29 16:49:27 +0200

    page admin

---
+++ admin.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+<div class="wrapper">
+        <!--Top menu -->
+        <div class="sidebar">
+           <!--profile image & text-->
+           <div class="profile">
+                <img src="assets/img/logoFondTransparent.png" alt="profile_picture">
+                <h3></h3>
+                <p></p>
+            </div>
+            <!--menu item-->
+            <ul>
+                <li>
+                    <a href="#" class="active">
+                        <span class="icon"><i class="fa-solid fa-house" ></i></span>
+                        <span class="item">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa-solid fa-briefcase"></i></span>
+                        <span class="item">Users</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa-solid fa-bookmark"></i></span>
+                        <span class="item">Etablissement</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa-solid fa-bookmark"></i></span>
+                        <span class="item">Participation</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa-solid fa-bookmark"></i></span>
+                        <span class="item">Impressions</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa-solid fa-bookmark"></i></span>
+                        <span class="item">Commentaire</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa-solid fa-bookmark"></i></span>
+                        <span class="item">Theme</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa-solid fa-bookmark"></i></span>
+                        <span class="item">Cursus</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        </div>
+
+    </div>
+</div>
+
+   
+</body>
+</html>
