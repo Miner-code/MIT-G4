a1fde9a6dcb724548faf7d073e5e80f77a1b9651
Warzaros <65943109+Warzaros@users.noreply.github.com>
2023-11-06 16:04:36 +0100

    profil update

--- profil.php
+++ profil.php
@@ -12,12 +12,39 @@ session_start();
 		include "include/bdd.php";
 		include "include/nav.php";
 		?>
-    <?php
-    $stmt = $bdd->prepare("SELECT * FROM user WHERE id_user = :id_user");
-    $stmt->bindParam(":id_user", $_SESSION['id_user']);
-    $stmt->execute();
-    $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
-    ?>
+
+		<div class="container">
+			<h1>Mon profil</h1>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Prénom</th>
+						<th>Nom</th>
+						<th>Email</th>
+						<th>Date de naissance</th>
+						<th>Niveau d'études</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$stmt = $bdd->prepare("SELECT * FROM user WHERE id_user = :id_user");
+					$stmt->bindParam(":id_user", $_SESSION['id_user']);
+					$stmt->execute();
+					while ($user_data = $stmt->fetch(PDO::FETCH_ASSOC)) {
+					?>
+						<tr>
+							<td><?php echo $user_data['firstname']; ?></td>
+							<td><?php echo $user_data['lastname']; ?></td>
+							<td><?php echo $user_data['email']; ?></td>
+							<td><?php echo $user_data['birthdate']; ?></td>
+							<td><?php echo $user_data['study']; ?></td>
+						</tr>
+					<?php
+					}
+					?>
+				</tbody>
+			</table>
+		</div>
 
 		<?php include "include/footer.php"; ?>
 	</body>
