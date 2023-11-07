695fdeb4fe403e7058e203d6a6474956b4a78ed6
Warzaros <65943109+Warzaros@users.noreply.github.com>
2023-11-07 15:14:37 +0100

    profil turns good

--- profil.php
+++ profil.php
@@ -19,38 +19,44 @@ session_start();
 	$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
 	?>
 
-	<h1 class="text-center">Mon profil</h1>
-	<div class="row justify-content-center">
-		<div class="col-md-4 d-flex align-items-center">
-			<i class="fas fa-user fa-5x me-3"></i>
-			<div>
-				<h4><?php echo $user_data['prenom_user'] . ' ' . $user_data['nom_user']; ?></h4>
-			</div>
-		</div>
-		<div class="col-md-6">
-			<div class="card">
-				<div class="card-body">
-					<table class="table">
-						<tbody>
-							<tr>
-								<td class="fs-4">Email</td>
-								<td class="fs-4"><?php echo $user_data['mail_user']; ?></td>
-							</tr>
-							<tr>
-								<td class="fs-4">Date de naissance</td>
-								<td class="fs-4"><?php echo $user_data['dtn_user']; ?></td>
-							</tr>
-							<tr>
-								<td class="fs-4">Niveau d'études</td>
-								<td class="fs-4"><?php echo $user_data['libelle_cursus']; ?></td>
-							</tr>
-						</tbody>
-					</table>
-					<a href="edit-profil.php" class="btn btn-primary"><i class="fas fa-pencil-alt"></i> Éditer</a>
-				</div>
-			</div>
-		</div>
-	</div>
+	<section class="d-flex justify-content-between">
+		<section class="rounded-circle p-3 bg-grey">
+            <svg height="36" viewBox="0 0 8 8" width="36" xmlns="http://www.w3.org/2000/svg">
+                <path d="m4 0c-1.1 0-2 1.12-2 2.5s.9 2.5 2 2.5 2-1.12 2-2.5-.9-2.5-2-2.5zm-2.09 5c-1.06.05-1.91.92-1.91 2v1h8v-1c0-1.08-.84-1.95-1.91-2-.54.61-1.28 1-2.09 1s-1.55-.39-2.09-1z"/>
+            </svg>
+        </section>
+		<h2><?= $user_data['prenom_user'].' '.$user_data['nom_user'] ?></h2>
+		<span style="width: calc(36px + 1rem)"></span>
+		<span style="width: calc(36px + 1rem)"></span>
+	</section>
+	<section class="d-flex justify-content-end">
+		<a href="edit-profil.php" class="btn btn-primary py-2 px-4">
+			<svg height="16" viewBox="0 0 8 8" width="16" fill="white	" xmlns="http://www.w3.org/2000/svg">
+				<path d="m6 0-1 1 2 2 1-1zm-2 2-4 4v2h2l4-4z"/>
+			</svg>
+			Éditer
+		</a>
+	</section>
+	<section class="row">
+		<section class="col-12 col-xl-6">
+			<label for="mail_user">Adresse mail :</label>
+			<input type="text" value="<?= $user_data['mail_user'] ?>" id="mail_user" class="form-control" disabled>
+		</section>
+		<section class="col-12 col-xl-6">
+			<label for="dtn_user">Date de naissance :</label>
+			<input type="text" value="<?= $user_data['dtn_user'] ?>" id="dtn_user" class="form-control" disabled>
+		</section>
+	</section>
+	<section class="row">
+		<section class="col-12 col-xl-6">
+			<label for="mail_user">Ecole :</label>
+			<input type="text" value="<?= $user_data['mail_user'] ?>" id="mail_user" class="form-control" disabled>
+		</section>
+		<section class="col-12 col-xl-6">
+			<label for="libelle_cursus">Niveau d'études :</label>
+			<input type="text" value="<?= $user_data['libelle_cursus'] ?>" id="libelle_cursus" class="form-control" disabled>
+		</section>
+	</section>
 
 	<?php include "include/footer.php"; ?>
 </body>
