fc687e39ae46e36adc1a59f2d342d00c78cf8410
Warzaros <65943109+Warzaros@users.noreply.github.com>
2023-11-07 14:26:26 +0100

    edit-profil release 1.3 & profil update

--- edit-profil.php
+++ edit-profil.php
@@ -14,8 +14,7 @@ session_start();
 	include "include/nav.php";
 	?>
 
-	<div class="container">
-		<h1>Modifier le profil</h1>
+		<h1 class="text-center">Modifier le profil</h1>
 		<?php
 		if (isset($_POST['update_profile'])) {
 			// Code pour mettre à jour le profil ici
@@ -23,48 +22,49 @@ session_start();
 		}
 		?>
 
-		<form method="post">
-			<table class="table">
-				<tbody>
-					<?php
-					$stmt = $bdd->prepare("SELECT * FROM user INNER JOIN participer ON user.id_user = participer.id_user INNER JOIN cursus ON cursus.id_cursus = participer.id_cursus WHERE user.id_user = :id_user");
-					$stmt->bindParam(":id_user", $_SESSION['id_user']);
-					$stmt->execute();
-					while ($user_data = $stmt->fetch(PDO::FETCH_ASSOC)) {
-						?>
-						<tr>
-							<td class="fs-4">Prénom</td>
-							<td class="fs-4"><input type="text" name="prenom_user" value="<?php echo $user_data['prenom_user']; ?>"></td>
-						</tr>
-						<tr>
-							<td class="fs-4">Nom</td>
-							<td class="fs-4"><input type="text" name="nom_user" value="<?php echo $user_data['nom_user']; ?>"></td>
-						</tr>
-						<tr>
-							<td class="fs-4">Email</td>
-							<td class="fs-4"><input type="email" name="mail_user" value="<?php echo $user_data['mail_user']; ?>"></td>
-						</tr>
-						<tr>
-							<td class="fs-4">Date de naissance</td>
-							<td class="fs-4"><input type="text" name="dtn_user" value="<?php echo $user_data['dtn_user']; ?>"></td>
-						</tr>
-						<tr>
-							<td class="fs-4">Niveau d'études</td>
-							<td class="fs-4">
-								<select name="cursus">
-									<option value="<?php echo $user_data['id_cursus']; ?>"><?php echo $user_data['libelle_cursus']; ?></option>
-									<!-- Vous devez charger la liste des cursus disponibles ici -->
-								</select>
-							</td>
-						</tr>
-						<?php
-					}
+	<form method="post">
+		<table class="table">
+			<tbody>
+				<?php
+				$stmt = $bdd->prepare("SELECT * FROM user INNER JOIN participer ON user.id_user = participer.id_user INNER JOIN cursus ON cursus.id_cursus = participer.id_cursus WHERE user.id_user = :id_user");
+				$stmt->bindParam(":id_user", $_SESSION['id_user']);
+				$stmt->execute();
+				while ($user_data = $stmt->fetch(PDO::FETCH_ASSOC)) {
 					?>
-				</tbody>
-			</table>
+					<tr>
+						<td>Prénom</td>
+						<td><input class="form-control" type="text" name="prenom_user" value="<?php echo $user_data['prenom_user']; ?>"></td>
+					</tr>
+					<tr>
+						<td>Nom</td>
+						<td><input class="form-control" type="text" name="nom_user" value="<?php echo $user_data['nom_user']; ?>"></td>
+					</tr>
+					<tr>
+						<td>Email</td>
+						<td><input class="form-control" type="email" name="mail_user" value="<?php echo $user_data['mail_user']; ?>"></td>
+					</tr>
+					<tr>
+						<td>Date de naissance</td>
+						<td><input class="form-control" type="text" name="dtn_user" value="<?php echo $user_data['dtn_user']; ?>"></td>
+					</tr>
+					<tr>
+						<td>Niveau d'études</td>
+						<td>
+							<select name="cursus" class="form-select">
+								<option value="<?php echo $user_data['id_cursus']; ?>"><?php echo $user_data['libelle_cursus']; ?></option>
+								<!-- Vous devez charger la liste des cursus disponibles ici -->
+							</select>
+						</td>
+					</tr>
+					<?php
+				}
+				?>
+			</tbody>
+		</table>
+		<section class="d-flex justify-content-center">
 			<button type="submit" name="update_profile" class="btn btn-primary">Mettre à jour le profil</button>
-		</form>
-	</div>
+		</section>
+	</form>
 
 	<?php include "include/footer.php"; ?>
 </body>
--- profil.php
+++ profil.php
@@ -12,45 +12,40 @@ session_start();
 	include "include/bdd.php";
 	include "include/nav.php";
 	?>
+	<?php
+	$stmt = $bdd->prepare("SELECT * FROM user INNER JOIN participer ON user.id_user = participer.id_user INNER JOIN cursus ON cursus.id_cursus = participer.id_cursus WHERE user.id_user = :id_user");
+	$stmt->bindParam(":id_user", $_SESSION['id_user']);
+	$stmt->execute();
+	$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+	?>
 
-	<div class="container">
-		<h1>Mon profil</h1>
-		<div class="row">
-			<div class="col">
-				<table class="table">
-					<tbody>
-						<?php
-						$stmt = $bdd->prepare("SELECT * FROM user INNER JOIN participer ON user.id_user = participer.id_user INNER JOIN cursus ON cursus.id_cursus = participer.id_cursus WHERE user.id_user = :id_user");
-						$stmt->bindParam(":id_user", $_SESSION['id_user']);
-						$stmt->execute();
-						while ($user_data = $stmt->fetch(PDO::FETCH_ASSOC)) {
-						?>
-							<tr>
-								<td class="fs-4">Prénom</td>
-								<td class="fs-4"><?php echo $user_data['prenom_user']; ?></td>
-							</tr>
-							<tr>
-								<td class="fs-4">Nom</td>
-								<td class="fs-4"><?php echo $user_data['nom_user']; ?></td>
-							</tr>
-							<tr>
-								<td class="fs-4">Email</td>
-								<td class="fs-4"><?php echo $user_data['mail_user']; ?></td>
-							</tr>
-							<tr>
-								<td class="fs-4">Date de naissance</td>
-								<td class="fs-4"><?php echo $user_data['dtn_user']; ?></td>
-							</tr>
-							<tr>
-								<td class="fs-4">Niveau d'études</td>
-								<td class="fs-4"><?php echo $user_data['libelle_cursus']; ?></td>
-							</tr>
-						<?php
-						}
-						?>
-					</tbody>
-				</table>
-			</div>
+	<h1 class="text-center">Mon profil</h1>
+	<div class="row">
+		<div class="col">
+			<table class="table">
+				<tbody>
+						<tr>
+							<td class="fs-4">Prénom</td>
+							<td class="fs-4"><?php echo $user_data['prenom_user']; ?></td>
+						</tr>
+						<tr>
+							<td class="fs-4">Nom</td>
+							<td class="fs-4"><?php echo $user_data['nom_user']; ?></td>
+						</tr>
+						<tr>
+							<td class="fs-4">Email</td>
+							<td class="fs-4"><?php echo $user_data['mail_user']; ?></td>
+						</tr>
+						<tr>
+							<td class="fs-4">Date de naissance</td>
+							<td class="fs-4"><?php echo $user_data['dtn_user']; ?></td>
+						</tr>
+						<tr>
+							<td class="fs-4">Niveau d'études</td>
+							<td class="fs-4"><?php echo $user_data['libelle_cursus']; ?></td>
+						</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 
