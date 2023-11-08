a5bdcb85a3ae9465adcd2c7f6bbf4e66d8ee98de
BenZeKing <benjamin84.guy@gmail.com>
2023-11-09 00:45:47 +0100

    arrondi + reforme du bouton envoie

--- view-post.php
+++ view-post.php
@@ -61,72 +61,84 @@ include "include/is-connected.php";
 			.image-arrondie{
 				border-radius: 50%;
 			}
+
+			.image-profileComm{
+				width: 40px;
+   				height: 40px;
+			}
+			.btn-carre {
+        		width: 40px;
+        		height: 40px; 
+        		border-radius: 8px;
+        		padding: 0;
+    		}
+			
     	</style>
-		<section class="d-flex justify-content-end w-100 mb-3">
-			<button class="btn btn-primary">
-				modifier
-			</button>
-		</section>
-			<?php } ?>
-		<section class="d-flex flex-column">
-			<section class="card p-2">
-				<section class="d-flex flex-row justify-content-between align-items-center">
-					<section class="rounded-circle p-3 width=36 height=36">
-						<img src="<?= $dataEtab['profil_etab'] ?>" class="image-reduite image-arrondie"/>
-					</section>
-					<h3 class="border-bottom border-dark"><?= $dataImpression['titre_imp'] ?></h3>
-					<span style="width: calc(36px + 1rem)"></span>
-				</section>
-				<section class="row">
-					<section class="col-2"></section>
-					<p class="col-8">
-						<?= $dataImpression['contenu_imp'] ?>
-					</p>
-					<section class="col-2"></section>
-				</section>
-				<section class="d-flex justify-content-end w-100 mb-3 pe-3">
-					<p><?= $dataUser['nom_user'].' '.$dataUser['prenom_user'].', '.$dataImpression['date_imp'] ?></p>
-					<!-- utiliser diffDate avec des split pour separer date et heure -->
-				</section>
+			<section class="d-flex justify-content-end w-100 mb-3">
+				<button class="btn btn-primary">
+					modifier
+				</button>
 			</section>
-		</section>
-			<section class="rounded-textarea d-flex justify-content-between mb-3">
-    			<textarea id="textAreaComm" name="textAreaComm" rows="4" placeholder="Répondre..."></textarea>
-				<section class="d-flex justify-content-end w-5 h-5 mb-2">
-					<button class="btn btn-primary mx-2 my-3" alt="Bouton Envoyer">
-						<svg class="align-items-center" fill="white" viewBox="0 0 16 16" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-							<path d="m16 1-1-1-15 6v2l7 1 1 7h2z">
-						</svg>
-    				</button>
-				</section>
-			</section>
-		<section class="d-flex flex-column">
-
-			<?php
-		for ($i=0; $i < count($dataComm); $i++) { 	
-			?>
-			<section>
-				<section class="d-flex flex-row justify-content-between align-items-center">
-					<section class="d-flex flex-row align-items-center">
-						<section class="rounded-circle p-2 bg-grey">
-							<svg height="36" viewBox="0 0 8 8" width="36" xmlns="http://www.w3.org/2000/svg">
-								<path d="m4 0c-1.1 0-2 1.12-2 2.5s.9 2.5 2 2.5 2-1.12 2-2.5-.9-2.5-2-2.5zm-2.09 5c-1.06.05-1.91.92-1.91 2v1h8v-1c0-1.08-.84-1.95-1.91-2-.54.61-1.28 1-2.09 1s-1.55-.39-2.09-1z"/>
-							</svg>				
+				<?php } ?>
+			<section class="d-flex flex-column">
+				<section class="card p-2 rounded-textarea">
+					<section class="d-flex flex-row justify-content-between align-items-center">
+						<section class="rounded-circle p-3 width=36 height=36">
+							<img src="<?= $dataEtab['profil_etab'] ?>" class="image-reduite image-arrondie"/>
 						</section>
-						<h4 class="fs-6"><?= $dataUser['nom_user'].' '.$dataUser['prenom_user'] ?></h4>
+						<h3 class="border-bottom border-dark"><?= $dataImpression['titre_imp'] ?></h3>
+						<span style="width: calc(36px + 1rem)"></span>
 					</section>
-					<p><?= publieDepuis($dataComm[$i]['date_com']) ?></p>
-					<!-- utiliser diffDate avec des split pour separer date et heure -->
-				</section>
-				<section class="mx-3 mb-5">
-					<?= $dataComm[$i]['contenu_com'] ?>
+					<section class="row">
+						<section class="col-2"></section>
+						<p class="col-8">
+							<?= $dataImpression['contenu_imp'] ?>
+						</p>
+						<section class="col-2"></section>
+					</section>
+					<section class="d-flex justify-content-end w-100 mb-3 pe-3">
+						<p><?= $dataUser['nom_user'].' '.$dataUser['prenom_user'].', '.$dataImpression['date_imp'] ?></p>
+						<!-- utiliser diffDate avec des split pour separer date et heure -->
+					</section>
 				</section>
 			</section>
+				<section class="rounded-textarea d-flex justify-content-between mb-3">
+					<textarea id="textAreaComm" name="textAreaComm" rows="4" placeholder="Répondre..."></textarea>
+					<section class="d-flex align-items-center justify-content-end w-5 h-5 mb-2">
+						<button class="btn btn-primary mx-2 my-3 btn-carre">
+							<svg class="align-items-center" fill="white" viewBox="0 0 16 16" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+								<path d="m16 1-1-1-15 6v2l7 1 1 7h2z"></path>
+							</svg>
+						</button>
+					</section>
+				</section>
+			<section class="d-flex flex-column">
 
-			<?php
-		}
-			?>
-		</section>
-		<?php require_once "include/footer.php"; ?>
+				<?php
+			for ($i=0; $i < count($dataComm); $i++) { 	
+				?>
+				<section>
+					<section class="d-flex flex-row justify-content-between align-items-center">
+						<section class="d-flex flex-row align-items-center">
+							<section class="rounded-circle p-2">
+								<img src="<?= $dataUser['img_user'] ?>" class="image-profileComm image-arrondie"/>
+							</section>
+							<h4 class="fs-6"><?= $dataUser['nom_user'].' '.$dataUser['prenom_user'] ?></h4>
+						</section>
+						<p><?= publieDepuis($dataComm[$i]['date_com']) ?></p>
+						<!-- utiliser diffDate avec des split pour separer date et heure -->
+					</section>
+					<section class="mx-3 mb-5">
+						<?= $dataComm[$i]['contenu_com'] ?>
+					</section>
+				</section>
+
+				<?php
+			}
+				?>
+			</section>
+			<?php require_once "include/footer.php"; ?>
 	</body>
 </html>
+
+		
